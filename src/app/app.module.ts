67036e8465325e1588ae81ffd95adadc91a4b201
pliniki <paulocarvalhojr2050@gmail.com>
2022-03-29 19:25:58 -0300

    consumo de api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { HttpClientModule } from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,6 +10,8 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { InicioComponent } from './inicio/inicio.component';
+
 
 @NgModule({
   declarations: [
@@ -16,11 +20,14 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
     FooterComponent,
     HomeComponent,
     EntrarComponent,
-    CadastrarComponent
+    CadastrarComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from '../model/UserLogin';
+import { Usuario } from '../model/usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -6,10 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastrar.component.css']
 })
 export class CadastrarComponent implements OnInit {
+  usuario: Usuario = new Usuario
+  confirmarSenha: string
+  tipoUsuario: string
 
-  constructor() { }
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    window.scroll(0, 0)
   }
 
+  confirmeSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value
+  }
+
+  cadastrarUsuario() {
+
+    this.usuario.tipo = this.tipoUsuario
+
+    if (this.usuario.senha != this.confirmarSenha) {
+      alert("Senhas incorretas")
+    } else {
+      this.authService.Cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp
+
+        alert('Usuario cadastrado com sucesso!')
+
+        this.router.navigate(['/entrar'])
+      })
+    }
+  }
 }
