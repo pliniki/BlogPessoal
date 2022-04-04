52c63d9e0d1ec632d4c29bc0e649e6c23fdef296
pliniki <paulocarvalhojr2050@gmail.com>
2022-04-04 17:56:59 -0300

    Criacao  dos endpoints de tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -9,7 +11,11 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+
   constructor(
+    private temaService: TemaService,
     private router: Router
   ) { }
 
@@ -18,6 +24,24 @@ export class TemaComponent implements OnInit {
 
       this.router.navigate(['/entrar'])
     }
+
+    this.findAllTema()
+
+  }
+
+  findAllTema() {
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar() {
+    this.temaService.postTema(this.tema).subscribe((resp: Tema) => {
+      this.tema = resp
+      alert('Tema cadastrado com sucesso!')
+      this.findAllTema()
+      this.tema = new Tema()
+    })
   }
 
 }
